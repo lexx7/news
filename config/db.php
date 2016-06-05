b4f536e3bc8ee104e203ca7f294eecc6189f6761
lex <lexx7@list.ru>
2016-06-05 22:28:52 +0300

    new project with yii2-users

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'Bdfy0db4',
+    'charset' => 'utf8',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
+];
