4fe155c063b978e007755a41fce649b58d2e0b01
Васянькин Алексей <vasyankin@f-technology.ru>
2017-08-09 15:04:39 +0300

    modify barcode reader

--- modules/barcode/views/default/index.php
+++ modules/barcode/views/default/index.php
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Show the barcode to the camera:
     </p>
 
     <div id="barcode">
